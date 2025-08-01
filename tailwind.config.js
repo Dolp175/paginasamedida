81c6e202bb95fed99d937489862cc4df8a478da0
Dolp175 <58003217+Dolp175@users.noreply.github.com>
2025-08-01 14:21:57 +0200

    Update SEO page palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,17 @@
 // tailwind.config.js
 module.exports = {
-  content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
   theme: {
     extend: {
-      display: ['group-hover'],
-      opacity: ['group-hover'],
-      transform: ['group-hover'],
+      display: ["group-hover"],
+      opacity: ["group-hover"],
+      transform: ["group-hover"],
       transitionDuration: {
-        '2000': '2000ms',
-        '3000': '3000ms',
-        '5000': '5000ms',
-      }
+        2000: "2000ms",
+        3000: "3000ms",
+        5000: "5000ms",
+      },
     },
   },
   plugins: [],
-}
-
+};
