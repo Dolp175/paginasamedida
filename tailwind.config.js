06f9d8ca6b443ed2c203ae89eb6257b495256fe6
Dolp175 <58003217+Dolp175@users.noreply.github.com>
2025-08-02 22:31:33 +0200

    refactor tailwind variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,18 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
   theme: {
     extend: {
-      display: ["group-hover"],
-      opacity: ["group-hover"],
-      transform: ["group-hover"],
       transitionDuration: {
         2000: "2000ms",
         3000: "3000ms",
         5000: "5000ms",
       },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["group-hover"],
+      opacity: ["group-hover"],
+      transform: ["group-hover"],
     },
   },
   plugins: [],
