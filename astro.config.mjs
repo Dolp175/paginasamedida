0486c64417ebdc5d2ab37eaa48214f402b122ec6
Jordi Martinez <jordi@MacStudio-de-Jordi.local>
2025-08-03 14:00:28 +0200

    first upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://www.paginasamedida.com",
+  integrations: [
+    tailwind(), 
+    sitemap({
+      filter: (page) => 
+        !page.includes('/gracias') && 
+        !page.includes('/legal/')
+    })
+  ]
 });
