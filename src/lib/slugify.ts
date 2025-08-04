23d133ccbf4cca5d761da37afe4930b24029867b
Dolp175 <58003217+Dolp175@users.noreply.github.com>
2025-08-05 01:45:22 +0200

    Use markdown headings with prose styling

--- src/lib/slugify.ts
+++
@@ -1,9 +0,0 @@
-export function slugify(str: string): string {
-  return str
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/[^a-z0-9\s-]/g, "")
-    .trim()
-    .replace(/\s+/g, "-");
-}
