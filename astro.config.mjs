423c390fcd8b2a7473944c159504979b329c6814
Jordi Martinez <jordi@MacStudio-de-Jordi.local>
2025-08-04 16:07:15 +0200

    mdx added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: "https://www.paginasamedida.com",
   integrations: [
+    mdx(),
     tailwind(), 
     sitemap({
       filter: (page) => 
